b21fb0c404391dbedbb5c170bd573c1ff42793ff
ramezaleem <ramez.aleem532@gmail.com>
2025-10-23 16:49:59 +0300

    Styled FrmViewResults with clean blue-gray theme and professional button design for viewing exam results

--- EmployeeExamSystem﻿/PL/AdminDahboard/Results/FrmViewResults.cs
+++ EmployeeExamSystem﻿/PL/AdminDahboard/Results/FrmViewResults.cs
@@ -2,6 +2,7 @@
 using EmployeeExamSystem_.PL.AdminDahboard.Results;
 using System;
 using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace EmployeeExamSystem_.PL.AdminDahboard
@@ -14,6 +15,29 @@ namespace EmployeeExamSystem_.PL.AdminDahboard
         {
             InitializeComponent();
             _resultsBL = new ResultsBL();
+            this.BackColor = ColorTranslator.FromHtml("#F8F9F9");
+
+            dgvExamsResults.BackgroundColor = Color.White;
+            dgvExamsResults.EnableHeadersVisualStyles = false;
+            dgvExamsResults.ColumnHeadersDefaultCellStyle.BackColor = ColorTranslator.FromHtml("#2E86C1");
+            dgvExamsResults.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvExamsResults.DefaultCellStyle.ForeColor = ColorTranslator.FromHtml("#34495E");
+            dgvExamsResults.DefaultCellStyle.SelectionBackColor = ColorTranslator.FromHtml("#5DADE2");
+            dgvExamsResults.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgvExamsResults.GridColor = ColorTranslator.FromHtml("#D6DBDF");
+
+            SetupButton(btnViewExamResults, "#2E86C1", "#5DADE2", "#1B4F72");
+        }
+        private void SetupButton(Button btn, string mainColor, string hoverColor, string borderColor)
+        {
+            btn.BackColor = ColorTranslator.FromHtml(mainColor);
+            btn.ForeColor = Color.White;
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderColor = ColorTranslator.FromHtml(borderColor);
+            btn.FlatAppearance.BorderSize = 1;
+
+            btn.MouseEnter += (s, e) => btn.BackColor = ColorTranslator.FromHtml(hoverColor);
+            btn.MouseLeave += (s, e) => btn.BackColor = ColorTranslator.FromHtml(mainColor);
         }
 
         private void FrmViewResults_Load(object sender, EventArgs e)
